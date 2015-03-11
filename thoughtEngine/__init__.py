d00d506ac04f81a494eb7d163c10402c730d87ff
Chandan <chandanchotu@gmail.com>
2015-03-11 15:09:02 +0530

    created Mongohq gv, and hosting mongo on remote host

--- thoughtEngine/__init__.py
+++ thoughtEngine/__init__.py
@@ -1,15 +1,31 @@
 from flask import Flask
-from flask.ext.mongoengine import MongoEngine
+from flask.ext.mongoengine import MongoEngine, connect
 from flask.ext.login import LoginManager
 
+import os
 
+MONGO_URL = os.environ.get("MONGOHQ_URL")
+
+
+if MONGO_URL:
+    credentials = re.sub(r"(.*?)//(.*?)(@hatch)", r"\2",MONGO_URL)
+    username = credentials.split(":")[0]
+    password = credentials.split(":")[1]
+    app.config["MONGODB_DB"] = MONGO_URL.split("/")[-1]
+    app.config["SECRET_KEY"] = "KeepThisS3cr3t"
+    connect(
+        MONGO_URL.split("/")[-1],
+        host=MONGO_URL,
+        port=29257,
+        username=username,
+        password=password
+    )
 
 app=Flask(__name__)
 
 
-app.config["MONGODB_SETTINGS"] = {'DB': "my_thought_log"}
+#app.config["MONGODB_SETTINGS"] = {'DB': "my_thought_log", 'host':'', 'port':'', 'user': '', 'password': ''}
 
-app.config["SECRET_KEY"] = "KeepThisS3cr3t"
 app.jinja_env.autoescape = False
 db = MongoEngine(app)
 
